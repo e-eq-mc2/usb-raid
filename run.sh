6e7f4c7a727fdf7651eec77842e88c278a6e6871
e-eq-mc2 <nk.nishizawa@gmail.com>
2017-05-30 19:33:39 +0900

    Remove the environment-specific code

--- run.sh
+++ run.sh
@@ -2,9 +2,4 @@
 
 sudo umount /dev/fuse
 
-rm -rf /media/n-nishizawa/a0aed6ed-7cb1-434d-8cf7-f7783666cb8d/*
-rm -rf data0
-rm -rf data1
-rm -rf data2
-
-bundle exec ruby ./bin/run.rb /media/n-nishizawa/NEKO_STORAGE
+bundle exec ruby ./bin/run.rb /path/to/the/mount/point
